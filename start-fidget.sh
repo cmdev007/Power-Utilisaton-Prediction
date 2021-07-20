0b19e0c56ba6732a85984bf6ae348665a5d21fc9
Preet Patel <preet.makz@gmail.com>
2021-07-20 14:48:43 +0530

    POWER-BACKEND: Unifyig script changes accoring to unifying watchman

--- start-fidget.sh
+++ start-fidget.sh
@@ -4,19 +4,22 @@ pkill python3.8
 cd backend/power/linearRegression/
 rm -rf LKEY.txt
 python3.8 runner.py&
-python3.8 watchman.py&
+# python3.8 watchman.py&
 cd ../../../
 
 cd backend/power/prototype_1/
 rm -rf LKEY.txt
 python3.8 runner.py
-python3.8 watchman.py&
+# python3.8 watchman.py&
 cd ../../../
 
 cd backend/power/prototype_2/
 rm -rf LKEY.txt
 python3.8 runner.py
-python3.8 watchman.py&  
-cd ../../../
+# python3.8 watchman.py&
+cd ../
+python3.8 watchman.py&
+
+cd ../..
 
 python3.8 manage.py runserver
